72899e8bfaa35d66b88f7f13e47a9fc7410c1558
Kelvin Soares <kelvin.soares@infomedbenner.com.br>
2019-10-09 16:35:49 -0300

    updated install-jenkins.sh and terrasg.tf

--- terrasg.tf
+++ terrasg.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "terra" {
 		from_port = 8080
 		to_port = 8080
 		protocol = "tcp"
-		cidr_blocks = ["${var.private_subnet_cidr}"]
+		cidr_blocks = ["0.0.0.0/0"]
 	}
 
 	egress {
