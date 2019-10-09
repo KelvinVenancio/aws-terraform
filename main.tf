f9546184ce873d18e341c906bedd1d940797b7f3
Kelvin Soares <kelvin.soares@infomedbenner.com.br>
2019-10-09 15:53:14 -0300

    added steps to install jenkins on docker

--- main.tf
+++ main.tf
@@ -27,16 +27,23 @@ resource "aws_instance" "terra" {
 		agent = true
 	}
 
+	provisioner "file" {
+		source      = "files/install-jenkins.sh"
+		destination = "/tmp/install-jenkins.sh"
+	}
+
 	provisioner "remote-exec" {
 	inline = [
 		"sudo apt-get update",
-		"sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+		"sudo apt-get install -y apt-transport-https ca-certificates curl git software-properties-common",
 		"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
 		"sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
 		"sudo apt-get update",
 		"sudo apt-get install docker-ce -y",
 		"sudo systemctl start docker",
-		"sudo systemctl enable docker"
+		"sudo systemctl enable docker",
+		"sudo chmod +x /tmp/install-jenkins.sh",
+		"sudo /tmp/install-jenkins.sh"
 	]
 }
 
