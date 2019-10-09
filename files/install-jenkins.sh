910f009d061fde2bbb40a4b9d02a45c21cbd343b
Kelvin Soares <kelvin.soares@infomedbenner.com.br>
2019-10-09 16:08:45 -0300

    updated install-jenkins.sh

--- files/install-jenkins.sh
+++ files/install-jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone git@github.com:KelvinVenancio/alpine-jenkins.git
+git clone https://github.com/KelvinVenancio/alpine-jenkins.git
 cd alpine-jenkins
 docker build -t jenkins_image .
 docker run -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock jenkins_image
