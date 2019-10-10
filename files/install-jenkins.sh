946151a457e46e1fa638d5ccf1d192cc4cb3a07c
Kelvin Soares <kelvinvenanciosoares@gmail.com>
2019-10-10 12:46:07 -0300

    updated script install-jenkins.sh

--- files/install-jenkins.sh
+++ files/install-jenkins.sh
@@ -7,11 +7,33 @@
 git clone https://github.com/KelvinVenancio/alpine-jenkins.git
 cd alpine-jenkins
 docker build -t jenkins_image .
+
+#
+# disable ufw and configure swap
+#
+
+systemctl stop ufw
+systemctl disable ufw
+iptables -F
+
+dd if=/dev/zero of=/swapfile count=1024 bs=1M
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+iptables -nL && free -m
+
+#
+# start the container
+#
+
+systemctl restart docker
 docker run -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock jenkins_image
 
 #
 # download jenkins-cli and test
 #
 
+sleep 30
 wget http://$(curl icanhazip.com):8080/jnlpJars/jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://$(curl icanhazip.com):8080/ list-jobs
